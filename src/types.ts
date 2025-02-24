51dbacf9ba78014be9db17c35c50918dadf59b69
Steve Liddle <liddle@byu.edu>
2025-02-24 09:04:33 -0700

    Built initial types to manage server API interactions

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export interface Book {
+    id: number;
+    abbr: string;
+    citeAbbr: string;
+    fullName: string;
+    numChapters: number;
+    urlPath: string;
+    parentBookId: number;
+    webTitle: string;
+    jstTitle: string;
+    tocName: string;
+    subdiv: string;
+    backName: string;
+    gridName: string;
+    citeFull: string;
+}
+
+export interface Books {
+    [key: string]: Book;
+}
+
+export interface Volume extends Book {
+    minBookId: number;
+    maxBookId: number;
+    books: Book[];
+}
